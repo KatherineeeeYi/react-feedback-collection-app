41b12a3ddc1b75be26571a160d4d5ce1fa7c43bb
yixuyu <yxy0921@vip.qq.com>
2018-10-01 16:11:07 -0400

    corection

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
 
-app.listen(5000);
+// 5000 in development, process.env.PORT in production
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
