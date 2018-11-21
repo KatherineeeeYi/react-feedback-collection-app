b33061f730a4c409d06070a256b5cc429421f485
yixuyu <yxy0921@vip.qq.com>
2018-11-21 10:11:02 -0500

    Added billing and client side app

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
     googleId: String,
-    name: String
+    credits: { type: Number, default: 0 }
 });
 
 // telling mongoose to create a collection called users
